981cf514bbe3ac82c2e6a76a837f925756c24082
Baytuch <alexander@it-hobby.km.ua>
2020-02-08 18:02:39 +0200

    Added font module

---
+++ src/font.h
@@ -0,0 +1,11 @@
+
+#ifndef FONT_H_INCLUDED
+#define FONT_H_INCLUDED
+
+#include <stdint.h>
+
+uint8_t getDataSize();
+void getCharData(const char ch, uint8_t *font_data);
+
+#endif // FONT_H_INCLUDED
+
