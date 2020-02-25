74c4fc249ccde10c8076d6e18e90970f2241cd05
Baytuch <alexander@it-hobby.km.ua>
2020-02-25 13:38:41 +0200

    Added interBi util (draw_utils)

--- src/draw_utils.cpp
+++ src/draw_utils.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <string.h>
 #include <jpeglib.h>
 #include "utils.h"
@@ -50,5 +51,57 @@ bool jpegToRaw(const char *img_path, uint8_t **img_area, uint32_t &img_width, ui
     fclose(img_file);
   }
   return res;
+}
+
+void interBi(const uint8_t *img_area_base, const uint32_t &img_width_base, const uint32_t &img_height_base,
+             uint8_t *img_area_com, const uint32_t &img_width_com, const uint32_t &img_height_com) {
+  for (uint32_t com_y = 0; com_y < img_height_com; com_y++) {
+    float tmp = 0.0;
+    tmp = static_cast<float>(com_y) / static_cast<float>(img_height_com - 1) * static_cast<float>(img_height_base - 1);
+    uint32_t base_c_y = static_cast<uint32_t>(floor(tmp));
+    if (base_c_y >= img_height_base - 1) {
+      base_c_y = img_height_base - 2;
+    }
+    const float u = tmp - static_cast<float>(base_c_y);
+    for (uint32_t com_x = 0; com_x < img_width_com; com_x++) {
+      tmp = static_cast<float>(com_x) / static_cast<float>(img_width_com - 1) * static_cast<float>(img_width_base - 1);
+      uint32_t base_c_x = static_cast<uint32_t>(floor(tmp));
+      if (base_c_x >= img_width_base - 1) {
+        base_c_x = img_width_base - 2;
+      }
+      const float t = tmp - static_cast<float>(base_c_x);
+      const float k_a = (1.0 - t) * (1.0 - u);
+      const float k_b = t * (1.0 - u);
+      const float k_c = t * u;
+      const float k_d = (1.0 - t) * u;
+      const uint8_t pix_a_r = img_area_base[base_c_x + base_c_y * img_width_base + img_width_base * img_height_base * 2];
+      const uint8_t pix_a_g = img_area_base[base_c_x + base_c_y * img_width_base];
+      const uint8_t pix_a_b = img_area_base[base_c_x + base_c_y * img_width_base + img_width_base * img_height_base];
+      const uint8_t pix_b_r = img_area_base[base_c_x + 1 + base_c_y * img_width_base + img_width_base * img_height_base * 2];
+      const uint8_t pix_b_g = img_area_base[base_c_x + 1 + base_c_y * img_width_base];
+      const uint8_t pix_b_b = img_area_base[base_c_x + 1 + base_c_y * img_width_base + img_width_base * img_height_base];
+      const uint8_t pix_c_r = img_area_base[base_c_x + 1 + (base_c_y + 1) * img_width_base + img_width_base * img_height_base * 2];
+      const uint8_t pix_c_g = img_area_base[base_c_x + 1 + (base_c_y + 1) * img_width_base];
+      const uint8_t pix_c_b = img_area_base[base_c_x + 1 + (base_c_y + 1) * img_width_base + img_width_base * img_height_base];
+      const uint8_t pix_d_r = img_area_base[base_c_x + (base_c_y + 1) * img_width_base + img_width_base * img_height_base * 2];
+      const uint8_t pix_d_g = img_area_base[base_c_x + (base_c_y + 1) * img_width_base];
+      const uint8_t pix_d_b = img_area_base[base_c_x + (base_c_y + 1) * img_width_base + img_width_base * img_height_base];
+      const uint8_t com_pix_r = static_cast<uint8_t>(static_cast<float>(pix_a_r) * k_a) +
+                                static_cast<uint8_t>(static_cast<float>(pix_b_r) * k_b) +
+                                static_cast<uint8_t>(static_cast<float>(pix_c_r) * k_c) +
+                                static_cast<uint8_t>(static_cast<float>(pix_d_r) * k_d);
+      const uint8_t com_pix_g = static_cast<uint8_t>(static_cast<float>(pix_a_g) * k_a) +
+                                static_cast<uint8_t>(static_cast<float>(pix_b_g) * k_b) +
+                                static_cast<uint8_t>(static_cast<float>(pix_c_g) * k_c) +
+                                static_cast<uint8_t>(static_cast<float>(pix_d_g) * k_d);
+      const uint8_t com_pix_b = static_cast<uint8_t>(static_cast<float>(pix_a_b) * k_a) +
+                                static_cast<uint8_t>(static_cast<float>(pix_b_b) * k_b) +
+                                static_cast<uint8_t>(static_cast<float>(pix_c_b) * k_c) +
+                                static_cast<uint8_t>(static_cast<float>(pix_d_b) * k_d);
+      img_area_com[com_x + com_y * img_width_com + img_width_com * img_height_com * 2] = com_pix_r;
+      img_area_com[com_x + com_y * img_width_com] = com_pix_g;
+      img_area_com[com_x + com_y * img_width_com + img_width_com * img_height_com] = com_pix_b;
+    }
+  }
 }
 
--- src/draw_utils.h
+++ src/draw_utils.h
@@ -5,5 +5,7 @@
 #include <stdint.h>
 
 bool jpegToRaw(const char *img_path, uint8_t **img_area, uint32_t &img_width, uint32_t &img_height);
+void interBi(const uint8_t *img_area_base, const uint32_t &img_width_base, const uint32_t &img_height_base,
+             uint8_t *img_area_com, const uint32_t &img_width_com, const uint32_t &img_height_com);
 
 #endif // DRAW_UTILS_H_INCLUDED
